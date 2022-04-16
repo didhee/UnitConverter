10f524ab2f5ca306051cd6a8cc7b6095ee988987
Onoriode Dafetta <onoriodedafetta@gmail.com>
2022-04-16 01:21:53 +0100

    fixed some unit errors

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ unitInput.addEventListener("input", () => {
 
         length.textContent = `${unitInput.value} ${unitInput.value > 1 ? units.meters : "meter"} = ${Math.round(unitInput.value * 3.281 * 1000) / 1000} feet | ${unitInput.value} ${unitInput.value > 1 ? units.feet : "feet"} = ${Math.round(unitInput.value * 3.305 * 1000) / 1000} meters`;
 
-        volume.textContent = `${unitInput.value} ${unitInput.value > 1 ? units.gallons : "gallons"} = ${Math.round(unitInput.value * 0.264 * 1000) / 1000} gallons | ${unitInput.value} ${unitInput.value > 1 ? units.liters : "liters"} = ${Math.round(unitInput.value * 4.546 * 1000) / 1000} liters`;
+        volume.textContent = `${unitInput.value} ${unitInput.value > 1 ? units.liters : "liters"} = ${Math.round(unitInput.value * 0.264 * 1000) / 1000} gallons | ${unitInput.value} ${unitInput.value > 1 ? units.gallons : "gallons"} = ${Math.round(unitInput.value * 4.546 * 1000) / 1000} liters`;
 
-        mass.textContent = `${unitInput.value} ${unitInput.value > 1 ? units.pounds : "pounds"} = ${Math.round(unitInput.value * 2.205 * 1000) / 1000} pounds | ${unitInput.value} ${unitInput.value > 1 ? units.kilograms : "kilograms"} = ${Math.round(unitInput.value * 0.454 * 1000) / 1000} kilograms`;
+        mass.textContent = `${unitInput.value} ${unitInput.value > 1 ? units.kilograms : "kilograms"} = ${Math.round(unitInput.value * 2.205 * 1000) / 1000} pounds | ${unitInput.value} ${unitInput.value > 1 ? units.pounds : "pounds"} = ${Math.round(unitInput.value * 0.454 * 1000) / 1000} kilograms`;
     }
 })
